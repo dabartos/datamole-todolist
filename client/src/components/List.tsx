32950212e2140487cf6946b1ae286f0f0e50e1b4
David Bartosik <david.bartosik@aimsapi.com>
2025-06-04 16:49:24 +0200

    [styling] add some constrictors to the list to fix components running off the screen

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const List = styled.div`
     display: flex;
     flex-direction: column;
+    margin-top: 10px;
+    overflow: auto;
+    max-height: 500px;
 `;
